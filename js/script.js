2eb3e88fcdeae78d3ad7a70a540431f5b040f951
wmcqueen9 <wentrellmcqueen80@gmail.com>
2024-10-28 16:15:07 -0500

    Added if else statements

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 var button=document.querySelector(".show-cat");
 var cat=document.querySelector(".cat");
 button.addEventListener("click", function(){
-cat.classList.add("show");
+if (cat.classList.contains("show")){
+    cat.classList.remove("show");
+    button.innerText="Wait, come back!";
+    button.classList.add("disappear");
+}
+else{
+    cat.classList.add("show");
+    button.innerText="Shoo, CAT!!";
+    button.classList.remove("disappear");
+}
 });
